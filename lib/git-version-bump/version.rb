e7a857fbe770f116fe677759a1caf8efcacef461
Matt Palmer <mpalmer@hezmatt.org>
2023-01-10 11:26:54 +1100

    Defer calculation of GVB version constants (relates to #22)
    I've never been particularly happy with how the versioning-related constants worked.
    With a bit of lateral thinking, it should now be a bit cleaner, at the expense of a bit of light const abuse.

--- lib/git-version-bump/version.rb
+++ lib/git-version-bump/version.rb
@@ -1,8 +1,33 @@
 module GitVersionBump
-	VERSION = GVB.version
-	MAJOR_VERSION = GVB.major_version
-	MINOR_VERSION = GVB.minor_version
-	PATCH_VERSION = GVB.patch_version
-	INTERNAL_REVISION = GVB.internal_revision
-	DATE = GVB.date
+	def self.VERSION
+		GVB.version
+	end
+
+	def self.MAJOR_VERSION
+		GVB.major_version
+	end
+
+	def self.MINOR_VERSION
+		GVB.minor_version
+	end
+
+	def self.PATCH_VERSION
+		GVB.patch_version
+	end
+
+	def self.INTERNAL_REVISION
+		GVB.internal_revision
+	end
+
+	def self.DATE
+		GVB.date
+	end
+
+	def self.const_missing(c)
+		if self.respond_to?(c) && c =~ /\A[A-Z_]+\z/
+			public_send c
+		else
+			super
+		end
+	end
 end
